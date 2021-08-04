356ff5aeeadea0b3e37a05034e1e25c748296291
npv34 <phanvanluan611996@gmail.com>
2021-08-04 10:42:48 +0700

    update router

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function (){
+   return redirect()->route('users.index');
+});
+
 Route::prefix('admin')->group(function () {
 
     Route::get('login', [\App\Http\Controllers\LoginController::class, 'showFormLogin'])->name('login');
