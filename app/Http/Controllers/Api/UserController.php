8d97c263bb569da833a51c51ce95394a6b26afe0
npv34 <phanvanluan611996@gmail.com>
2021-12-27 15:18:53 +0700

    create api crud user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -35,5 +35,53 @@ class UserController extends Controller
         return response()->json($data);
     }
 
+    function delete($id) {
+        $user = User::find($id);
+        $user->roles()->detach();
+        $user->delete();
+        $data = [
+            "status" => "success",
+            "message" => "Delete user successfully"
+        ];
+        return response()->json($data);
+    }
+
+    function findById($id) {
+        $user = User::with('roles')->find($id);
+        if (!$user){
+            $data = [
+                "status" => "error",
+                "message" => "User not found!"
+            ];
+            return response()->json($data);
+        }
+        $data = [
+            "status" => "success",
+            "user" => $user
+        ];
+        return response()->json($data);
+    }
+
+    function update(Request $request, $id) {
+        $user = User::find($id);
+        if (!$user){
+            $data = [
+                "status" => "error",
+                "message" => "User not found!"
+            ];
+            return response()->json($data);
+        }
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+
+        //them role user
+        $user->roles()->sync($request->roles);
+        $data = [
+            "status" => "success",
+            "message" => "Update user successfully"
+        ];
+        return response()->json($data);
+    }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('users', [UserController::class,'getAll']);
 Route::post('users', [UserController::class,'store']);
+Route::delete('users/{id}', [UserController::class, 'delete']);
+Route::get('users/{id}', [UserController::class, 'findById']);
+Route::put('users/{id}', [UserController::class, 'update']);
+
 
 Route::prefix('roles')->group(function (){
     Route::get('', [RoleController::class, 'index']);
